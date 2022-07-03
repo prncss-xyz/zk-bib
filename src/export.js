9264cb3a2833ad5672286800b85a4658dcc98ab8
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2022-07-03 19:06:34 -0400

    perf: make exports happen in parallel

--- src/export.js
+++ src/export.js
@@ -6,7 +6,7 @@ import { zk, mk, sdrPath } from "./utils.js";
 
 export default async function export_(options) {
   const plan = {};
-  for (let [_, dir] of Object.entries(config.export)) {
+  const promFrom = Object.entries(config.export).map(async ([_, dir]) => {
     dir = path.resolve(process.env.HOME, dir);
     await fs.mkdir(dir, { recursive: true });
     const entries = await fs.readdir(dir);
@@ -17,51 +17,65 @@ export default async function export_(options) {
         plan[entry].from = path.join(dir, entry);
       }
     }
-  }
-  for (let [tag, dir] of Object.entries(config.export)) {
-    const res = await zk([
-      "list",
-      "--tag",
-      tag,
-      "--format",
-      "{{abs-path}}",
-      config.noteDir,
-    ]);
-    for (const filePath of res.split("\n")) {
-      if (filePath === "") continue;
-      const raw = await fs.readFile(filePath, "utf-8");
-      const data = matter(raw).data;
-      let dest = data.asset;
-      const ext = path.extname(dest);
-      if (ext === ".html" || ext === ".ext") {
-        dest = path.basename(dest, ext) + ".epub";
-      } else dest = path.basename(dest);
-      plan[dest] ??= {};
-      plan[dest].to_ = path.resolve(process.env.HOME, dir);
-      plan[dest].data = data;
+  });
+  const promTo = Object.entries(config.export).map(async ([tag, dir]) => {
+    {
+      const res = await zk([
+        "list",
+        "--tag",
+        tag,
+        "--format",
+        "{{abs-path}}",
+        config.noteDir,
+      ]);
+      for (const filePath of res.split("\n")) {
+        if (filePath === "") continue;
+        const raw = await fs.readFile(filePath, "utf-8");
+        const data = matter(raw).data;
+        let dest = data.asset;
+        const ext = path.extname(dest);
+        if (ext === ".html" || ext === ".ext") {
+          dest = path.basename(dest, ext) + ".epub";
+        } else dest = path.basename(dest);
+        plan[dest] ??= {};
+        plan[dest].to_ = path.resolve(process.env.HOME, dir);
+        plan[dest].data = data;
+      }
     }
-  }
-  for (const [filename, { from, to_, data }] of Object.entries(plan)) {
-    if (from && to_) {
-      if (from !== path.join(to_, filename)) {
-        if (options.dryRun) console.log("mv", from, path.join(to_, filename));
-        else {
-          await fs.rename(from, path.join(to_, filename));
-          try {
-            await fs.rename(sdrPath(from), sdrPath(path.join(to_, filename)));
-          } catch (e) {
-            // TODO: rethrow
+  });
+  await Promise.all(promFrom);
+  await Promise.all(promTo);
+
+  const promDo = Object.entries(plan).map(
+    async ([filename, { from, to_, data }]) => {
+      {
+        if (from && to_) {
+          if (from !== path.join(to_, filename)) {
+            if (options.dryRun)
+              console.log("mv", from, path.join(to_, filename));
+            else {
+              await fs.rename(from, path.join(to_, filename));
+              try {
+                await fs.rename(
+                  sdrPath(from),
+                  sdrPath(path.join(to_, filename))
+                );
+              } catch (e) {
+                // TODO: rethrow
+              }
+            }
           }
+        } else if (from) {
+          if (options.dryRun) console.log("rm", from);
+          else await fs.rm(from);
+        } else if (to_) {
+          if (options.dryRun) console.log("mk", path.join(to_, filename));
+          else await mk(data, to_);
+        } else {
+          assert(false);
         }
       }
-    } else if (from) {
-      if (options.dryRun) console.log("rm", from);
-      else await fs.rm(from);
-    } else if (to_) {
-      if (options.dryRun) console.log("mk", path.join(to_, filename));
-      else await mk(data, to_);
-    } else {
-      assert(false);
     }
-  }
+  );
+  await Promise.all(promDo);
 }
